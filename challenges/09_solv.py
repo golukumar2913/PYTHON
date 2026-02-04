6dce0295e923c6b48b901db2ddbc5df971f22fb1
golukumar2913 <gkuma556@gmail.com>
2026-02-04 22:50:42 +0530

    new

--- challenges/09_solv.py
+++ challenges/09_solv.py
@@ -1,11 +1,12 @@
-num = int(input("Enter a number: "))
+num = int(input("Enter your no "))
 
 reverse = 0
 
-while num > 0:
+while num > 0 :
     digit = num % 10
     reverse = reverse * 10 + digit
+    # num //= 10
     num = num // 10
 
-print(reverse)
 
+print(reverse)

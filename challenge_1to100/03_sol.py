bd03a61d4a111956461b356fed2e4693e80c8438
golukumar2913 <gkuma556@gmail.com>
2026-03-16 21:56:10 +0530

    Challenge

---
+++ challenge_1to100/03_sol.py
@@ -0,0 +1,12 @@
+# Find largest number
+
+first = 34
+sec = 78
+third = 23
+
+if first >= sec and first >= third:
+    print(first)
+elif sec >= first and sec >= third:
+    print(sec)  
+else:
+    print(third)    

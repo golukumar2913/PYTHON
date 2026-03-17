dd627a7b9f7aa468e8ba4b34e1c1ce2b8fc78e7c
golukumar2913 <gkuma556@gmail.com>
2026-03-18 00:35:11 +0530

    challenge

---
+++ challenge_1to100/08_solv.py
@@ -0,0 +1,11 @@
+# Simple Calculator
+
+first_no = int(input("Enter 1st No "))
+sec_no = int(input("Enter 2nd No "))
+
+print("Add no = ", first_no + sec_no)
+print("Minus no = ", first_no - sec_no)
+print("Multiplication no = ", first_no * sec_no)
+print("division no = ", first_no / sec_no)
+print("Mudulo no = ", first_no % sec_no)
+print("Floor no = ", first_no // sec_no)

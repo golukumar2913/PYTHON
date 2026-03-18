db55f1047a116ce34fd9740eaa597192b1f4fb32
golukumar2913 <gkuma556@gmail.com>
2026-03-19 02:02:34 +0530

    challenge

---
+++ challenge_1to100/13_sol.py
@@ -0,0 +1,9 @@
+#  celsius to  fahrenheit and  fahrenheit to celsius
+
+temp = int(input("Enter Temp in celsius "))
+
+fahren = (temp * 9/5) + 32 
+print(fahren,"°F")
+
+celsious = (temp - 32) * 5/9
+print(celsious ,"°C")

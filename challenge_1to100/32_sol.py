1e30d5b1470de1042200819783380388d02e6c64
golukumar2913 <gkuma556@gmail.com>
2026-03-28 12:01:08 +0530

    challenge

---
+++ challenge_1to100/32_sol.py
@@ -0,0 +1,15 @@
+num = int(input("Enter no "))
+
+temp = num 
+digits = len(str(num))
+total = 0
+
+while temp > 0:
+    digit = temp % 10
+    total += digit ** digits
+    temp //= 10
+print(total)
+if num == total :
+    print("Armstrong number")
+else:
+    print("Not Armstrong")       

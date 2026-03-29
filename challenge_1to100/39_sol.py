00c87dcdea68962cc6cfdcf9fc9e1d6b9714937e
golukumar2913 <gkuma556@gmail.com>
2026-03-29 23:53:15 +0530

    challenge

---
+++ challenge_1to100/39_sol.py
@@ -0,0 +1,11 @@
+# convert binary to decimal no 
+
+binary = input("Enter binary no ")
+
+# result = 0 
+# for digit in binary:
+#     result = result * 2 + int(digit)
+
+# print(result)    
+
+print(int(binary, 2))

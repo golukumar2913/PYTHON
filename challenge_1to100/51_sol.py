fb7ab1eef9f94bb9043ca7c7e54f81f114706422
golukumar2913 <gkuma556@gmail.com>
2026-04-02 23:07:59 +0530

    Reverse Pyramid

---
+++ challenge_1to100/51_sol.py
@@ -0,0 +1,9 @@
+# Reverse Pyramid
+
+n = int(input("Enter no "))
+
+for i in range(n, 0 , -1 ):
+    print(" " * (n - i) ,end="")
+    for j in range( i):
+        print("*", end=" ")
+    print()    

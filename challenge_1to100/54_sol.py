4d9c02b2fde58da14766d9c941c2e75334734cd9
golukumar2913 <gkuma556@gmail.com>
2026-04-05 21:03:32 +0530

    Sandglass Pattern

--- challenge_1to100/54_sol.py
+++ challenge_1to100/54_sol.py
@@ -1,16 +1,16 @@
+# Sandglass Pattern
+
 
 n = int(input("Enter number: "))
 
 for i in range(n, 0 , -1):
     print(" "*(n - i), end="")
-
     for j in range (i):
         print("*", end=" ")
     print()  
 
 for i in range (2 , n +1):
     print(" " * (n - i), end="")
-
     for j in range(i) :
         print("*", end=" ")
     print()         

cbf88552dd8b46e52f8b4d4a911e15c0b22765d9
golukumar2913 <gkuma556@gmail.com>
2026-04-05 21:25:20 +0530

    Butterfly Pattern

---
+++ challenge_1to100/55_sol.py
@@ -0,0 +1,13 @@
+# Butterfly Pattern
+
+n = int(input("Enter no "))
+
+for i in range(1, n+1):
+    print("*" * i , end="")
+    print(" "*(2* (n - i )),end="")
+    print("*" * i  )
+
+for i in range(n , 0 , -1):
+    print("*" * i , end="")
+    print(" "*(2* (n - i )),end="")
+    print("*" * i  )    

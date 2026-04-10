8bed8dd6f2c67d2a24bf3c10fcc9242b7c9a1efb
golukumar2913 <gkuma556@gmail.com>
2026-04-10 15:31:46 +0530

    Captalize first letter

---
+++ challenge_1to100/66_sol.py
@@ -0,0 +1,13 @@
+# Captalize first letter 
+
+s = input("Enter string ")
+
+first = s.capitalize()
+sec = s.title()
+third = s.upper()
+four = s[0].upper()+ s[1:]
+
+print(first)
+print(sec)
+print(third)
+print(four)

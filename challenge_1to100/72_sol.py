4a43c79bed97f5ba52b907f05bb39e83cf269ce5
golukumar2913 <gkuma556@gmail.com>
2026-04-12 16:03:35 +0530

    Largest element

---
+++ challenge_1to100/72_sol.py
@@ -0,0 +1,15 @@
+# Largest Element 
+
+lit = list(map(int, input("Enter no ").split()))
+
+largest = lit[0]   
+
+for i in lit:
+    if i > largest:
+        largest = i
+
+print("Largest:", largest)
+
+# Short method 
+
+print("Largest ", max(lit))

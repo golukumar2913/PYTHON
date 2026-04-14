bb2ec5197d0d70fb04f9745f241b5f43180e74e2
golukumar2913 <gkuma556@gmail.com>
2026-04-14 23:39:58 +0530

    merge list

---
+++ challenge_1to100/78_sol.py
@@ -0,0 +1,28 @@
+# Merge Two list
+
+arr1 = list(map(int, input("Enter first list ").split()))
+arr2 = list(map(int, input("Enter 2nd list ").split()))
+
+
+i = 0
+j = 0
+
+mergelist = []
+
+while i < len(arr1) and j < len(arr2):
+    if arr1[i] < arr2[j]:
+        mergelist.append(arr1[i])
+        i += 1
+    else:
+        mergelist.append(arr2[j])
+        j += 1
+while i < len(arr1):
+    mergelist.append(arr1[i])
+    i += 1
+while j < len(arr2):
+    mergelist.append(arr2[j])  
+    j += 1
+  
+print(mergelist)
+
+
